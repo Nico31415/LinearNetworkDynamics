5ff2b15fbf99dbd930b72c3fc7bdb7d0f3426467
Nico31415 <nicoguitarrazurda@gmail.com>
2024-06-19 23:31:38 +0100

    comments

--- accuracy_lambda.py
+++ accuracy_lambda.py
@@ -4,9 +4,11 @@ import numpy as np
 from qqt_lambda_balanced import QQT_lambda_balanced
 import matplotlib.pyplot as plt
 
+# Set training parameters
 training_steps = 200
 learning_rate = 0.0001
 
+# Define a range of lambda values and network sizes
 lmdas = np.logspace(np.log10(0.01), np.log10(100), 20)
 network_sizes = [3, 5, 10, 20]
 
@@ -14,14 +16,17 @@ losses_analytical = dict()
 losses_empirical = dict()
 loss_deviations = dict()
 
+# Set the number of trials for each experiment
 num_tries = 20
 
+# Function to calculate the difference between analytical and empirical losses
 def loss_difference(loss_anal, loss_emp):
     return np.mean((loss_anal - loss_emp) / loss_emp)
     # return np.mean((loss_anal - loss_emp))
 
 for network_size in network_sizes:
 
+    # Set dimensions based on network size (equal input output hidden dimensions)
     in_dim = network_size 
     hidden_dim = network_size
     out_dim = network_size 
@@ -45,15 +50,18 @@ for network_size in network_sizes:
             model = LinearNetwork(in_dim, hidden_dim, out_dim, init_w1, init_w2)
             w1s, w2s, _ = model.train(X, Y, training_steps, learning_rate)
 
+            # Calculate empirical loss
             loss = [1/(2 * batch_size) * np.linalg.norm(w2 @ w1 @ X - Y)**2 for (w1, w2) in zip(w1s, w2s)]
             losses_empirical[network_size][lmda][n] = loss
 
+            # Calculate analytical loss using QQT_lambda_balanced
             analytical = QQT_lambda_balanced(init_w1.copy(), init_w2.copy(), X.T, Y.T, True)
             analytical = [analytical.forward(learning_rate) for _ in range(training_steps)]
             
             loss_analytical = np.array([1/(2*batch_size) * np.linalg.norm(w @ X - Y)**2 for w in analytical])
             losses_analytical[network_size][lmda][n] = loss_analytical
 
+            # Calculate loss deviation
             loss_deviations[network_size][lmda][n] = loss_difference(loss_analytical, loss)
 
 plt.figure(figsize=(10, 6))
@@ -81,9 +89,3 @@ plt.legend(fontsize=10)
 plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 plt.tight_layout()
 plt.show()
-
-
-
-
-
-
